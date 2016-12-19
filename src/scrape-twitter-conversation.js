2c6b438131675d7a4dfdee14481efa82d8c55e1f
Seb Insua <me@sebinsua.com>
2016-12-19 16:27:23 +0000

    fix(cli): twitter conversation cli fixed

--- src/scrape-twitter-conversation.js
+++ src/scrape-twitter-conversation.js
@@ -12,12 +12,18 @@ const cli = meow(`
   string: [ '_' ] // It turns out Twitter ids are very large...
 })
 
-if (cli.input.length < 2) {
+if (cli.input.length === 0) {
   cli.showHelp()
 } else {
-  const username = cli.input[0]
-  const id = cli.input[1]
-  const tweets = username ? new ConversationStream(username, id) : new ThreadedConversationStream(id)
+  let tweets
+  if (cli.input.length >= 2) {
+    const username = cli.input[0]
+    const id = cli.input[1]
+    tweets = new ConversationStream(username, id)
+  } else {
+    const id = cli.input[0]
+    tweets = new ThreadedConversationStream(id)
+  }
   pump(tweets, JSONStream.stringify('[\n', ',\n', '\n]\n'), process.stdout, err => {
     if (err != null) {
       if (err.statusCode !== 404) {
